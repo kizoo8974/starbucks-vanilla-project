6f9c549efaacadf4336b990d9c2f17f3220105bf
Joon Kim <kizoo8974@gmail.com>
2023-02-02 15:16:02 -0800

    Visual images fade in

--- js/main.js
+++ js/main.js
@@ -34,3 +34,12 @@ window.addEventListener('scroll', _.throttle(function () {
     }
 }, 300));
 // _.throttle(function, mil.sec)
+
+// Visual images fade in 
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+    gsap.to(fadeEl, 1, {
+        delay: (index + 1) *.7,
+        opacity: 1
+    });
+});
