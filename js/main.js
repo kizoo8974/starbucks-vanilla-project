325385e99396b643ea78255ebd8f8beb908220a7
Joon Kim <kizoo8974@gmail.com>
2023-02-04 20:26:28 -0800

    notice slide - swiper.js

--- js/main.js
+++ js/main.js
@@ -43,3 +43,11 @@ fadeEls.forEach(function (fadeEl, index) {
         opacity: 1
     });
 });
+
+// Slide - Swiper.js
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical',
+    autoplay: true,
+    loop: true
+});
+
