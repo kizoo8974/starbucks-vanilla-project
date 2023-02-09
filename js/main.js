46ca8f2cb70c8ce9070b80e1a690ef455f8a9bac
Joon Kim <kizoo8974@gmail.com>
2023-02-08 21:07:57 -0800

    footer

--- js/main.js
+++ js/main.js
@@ -69,7 +69,17 @@ new Swiper('.promotion .swiper-container', {
         nextEl: '.promotion ,swiper-next'
     }
 });
+new Swiper('.awards .swiper-container', {
+    autoplay: true,
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 5,
+    navigation: {
+        prevEl: '.awards .swiper-prev',
+        nextEl: '.awards .swiper-next'
+    }
 
+});
 
 
 const promotionEl = document.querySelector('.promotion');
@@ -118,3 +128,7 @@ spyEls.forEach(function (spyEl) {
         .setClassToggle(spyEl,  'show')
         .addTo(new ScrollMagic.Controller());
 });
+
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
